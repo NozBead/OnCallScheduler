7c0dfe2b3a1bbc5eeb13bd68adfc49409fb96e63
Enzo Pain <enzo.pain.etu@univ-lille.fr>
2023-04-15 22:56:47 +0200

    add split, add alternating

--- src/main/java/fr/funetdelire/oncallscheduler/config/OnCallSchedulerConfig.java
+++ src/main/java/fr/funetdelire/oncallscheduler/config/OnCallSchedulerConfig.java
@@ -2,18 +2,17 @@ package fr.funetdelire.oncallscheduler.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "fr.funetdelire.oncallscheduler")
-@EnableWebMvc
+@Configuration 
 public class OnCallSchedulerConfig implements WebMvcConfigurer{
-
 	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
-    }
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addViewController("/")
+	    .setViewName("forward:scheduler.html");
+	}
 }
